e1b55aa9d1ae45886876fa03f3076cade2f363f8
Rudra340 <rudra.sce21@sot.pdpu.ac.in>
2024-04-03 16:56:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
+export default function ReactHookFormMini() {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+  const { formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    setData(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full flex justify-center items-center bg-gray-900 p-8 border-r border-dashed">
+      <div className="w-1/2 shadow-lg rounded-md bg-white p-8 flex flex-col" style={{ height: '375px' }}>
+        <h2 className="text-center font-medium text-2xl mb-4">
+          React Hook Form abc
+        </h2>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-1 flex-col justify-evenly"
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            className="border-2 outline-none p-2 rounded-md"
+            placeholder="Name"
+            {...register("name")}
+          />
+          <input
+            className="border-2 outline-none p-2 rounded-md"
+            placeholder="Phone Number"
+            {...register("phoneNumber", {
+              minLength: 1,
+              maxLength: 10,
+            })}
+          />
+          <input
+            className="border-2 outline-none p-2 rounded-md"
+            placeholder="Email"
+            {...register("email", {
+              pattern: {
+                value: /^.*@gmail.com$/,
+                message: 'Email must end with gmail.com'
+              }
+            })}
+          />
+          <button
+            className=" flex justify-center p-2 rounded-md w-1/2 self-center bg-gray-900  text-white hover:bg-gray-800"
+            type='submit'
+          >
+            <span>
+            Submit
+              {errors?.email?.message}
+            </span>
+          </button>
+        </form>
+        <div className='h-4'>
+          <p> Data: {JSON.stringify(data)}</p>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
